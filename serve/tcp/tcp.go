75bf9a83752baa98fbcaafa7f697e18ef409bd84
dengxiaochuan <1554694323@qq.com>
2018-12-28 23:48:24 +0800

    add proto

--- serve/tcp/tcp.go
+++ serve/tcp/tcp.go
@@ -1,8 +1,14 @@
 package tcp
 
 import (
+	"bytes"
+	"encoding/binary"
 	"fmt"
 	"net"
+
+	"github.com/golang/protobuf/proto"
+
+	"github.com/Justyer/JekoServer/model/auth"
 
 	"github.com/Justyer/JekoServer/model"
 )
@@ -15,6 +21,8 @@ func TCPServe() {
 		return
 	}
 	defer listener.Close()
+
+	fmt.Printf("[listener]: (%s)\n\n", model.TCPIPort)
 
 	for {
 		conn, err := listener.Accept()
@@ -40,40 +48,52 @@ func TCPServe() {
 // 每个client连接后的处理
 func JustDoIt(conn net.Conn) {
 	defer conn.Close()
-	// buf_pool := make([]byte, 1024)
-	// for {
-	// 	buf := make([]byte, 512)
-	// 	l, err := conn.Read(buf)
-	// 	if err != nil {
-	// 		fmt.Printf("[Error reading]: %s, on (%s)\n", err.Error(), conn.RemoteAddr().String())
-	// 		return
-	// 	}
+	buf_pool := make([]byte, 1024)
+	for {
+		buf := make([]byte, 512)
+		l, err := conn.Read(buf)
+		if err != nil {
+			fmt.Printf("[Error reading]: %s, on (%s)\n", err.Error(), conn.RemoteAddr().String())
+			return
+		}
 
-	// 	// 将本次读取的字节放到字节池里
-	// 	for _, b := range buf {
-	// 		buf_pool = append(buf_pool, b)
-	// 	}
+		// 将本次读取的字节放到字节池里
+		for _, b := range buf {
+			buf_pool = append(buf_pool, b)
+		}
 
-	// 	var datapack []byte
+		var datapack []byte
 
-	// 	// 如果字节长度大于6，才可能是完整的数据包（不是一定）
-	// 	if l > 6 {
-	// 		var dl int
-	// 		dl_buf := bytes.NewBuffer(buf[4:6])
-	// 		binary.Write(dl_buf, binary.BigEndian, &dl)
-	// 		// 如果缓冲区接收的字节长度大于等于数据包长度，处理该数据
-	// 		if l >= dl+6 {
-	// 			datapack = buf[:dl+6]
+		// 如果字节长度大于6，才可能是完整的数据包（不是一定）
+		if l > 8 {
+			var dl int
+			dl_buf := bytes.NewBuffer(buf[4:8])
+			binary.Write(dl_buf, binary.BigEndian, &dl)
+			// 如果缓冲区接收的字节长度大于等于数据包长度，处理该数据
+			if l >= dl+8 {
+				datapack = buf[:dl+8]
 
-	// 			// 大分类
-	// 			big_cate := datapack[0:2]
-	// 			// 小分类
-	// 			sml_cate := datapack[4:4]
+				// 大分类
+				big_cate := bytes.NewBuffer(datapack[0:2])
+				// 小分类
+				sml_cate := bytes.NewBuffer(datapack[4:6])
 
-	// 			// 数据
-	// 			data := datapack[6 : dl+6]
-	// 		}
-	// 	}
+				// 数据
+				data := datapack[6 : dl+8]
 
-	// }
+				var big_cate_short uint16
+				binary.Write(big_cate, binary.BigEndian, &big_cate_short)
+
+				var sml_cate_short uint16
+				binary.Write(sml_cate, binary.BigEndian, &sml_cate_short)
+
+				var req auth.Login
+				proto.Unmarshal(data, &req)
+
+				fmt.Println("recv:", big_cate_short, sml_cate_short, req)
+
+			}
+		}
+
+	}
 }
