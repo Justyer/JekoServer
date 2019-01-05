571bec7abadb08ac585ff43b17b5f83b6eddb38d
dengxiaochuan <1554694323@qq.com>
2019-01-05 18:23:30 +0800

    vendor

---
+++ vendor/Groot/src/golibs/db/mysql/gorm/mysql.go
@@ -0,0 +1,28 @@
+package mysql
+
+import (
+	"fmt"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"log"
+)
+
+type DBInfo struct {
+	User   string
+	Pass   string
+	Host   string
+	Port   int
+	DbName string
+}
+
+func Conn(dbi DBInfo) *gorm.DB {
+	dbLinkTemp := "%s:%s@tcp(%s:%d)/%s"
+	dbLink := fmt.Sprintf(dbLinkTemp,
+		dbi.User, dbi.Pass, dbi.Host,
+		dbi.Port, dbi.DbName)
+	db, err := gorm.Open("mysql", dbLink)
+	if err != nil {
+		log.Fatal("[connect mysql err]: ", err)
+	}
+	return db
+}
