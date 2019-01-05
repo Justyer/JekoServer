a5dff706a1f9cae88bcd255588d836c5700ba7dd
thanklzhang <940678686@qq.com>
2019-01-05 17:22:45 +0800

    add config folder

---
+++ config_data/model/Test.go
@@ -0,0 +1,38 @@
+     
+type Test struct {
+    //名称
+        Name string;
+        
+        //描述
+        Description string;
+        
+        //备注
+        Comment string;
+        
+        //图标
+        Icon string;
+        
+        //最大数量
+        MaxNumber int;
+        
+        //能否使用
+        CanUse bool;
+        
+        //能否显示在背包
+        CanShowInBag bool;
+        
+        //是否礼包
+        IsPackage bool;
+        
+        //类型1（0-无，1-英雄碎片，2-强化材料，3-工坊制品，4-基础材料，5-礼包）
+        Type1 int;
+        
+        //类型2
+        Type2 int;
+        
+        //类型3
+        Type3 int;
+        
+        
+	
+}
